dfbf0a986cf65bcdf439353666c02d343c39ce83
Oliver Forral <intrepion@gmail.com>
2025-06-14 20:27:15 -0700

    Updating test file to C++23.

--- test.cpp
+++ test.cpp
@@ -1,10 +1,32 @@
 #include <gtest/gtest.h>
 
-TEST(MyTestSuite, MyTestCase) {
+#include <print>
+#include <string_view>
+
+using std::println;
+using std::string_view;
+
+TEST(MathTestSuite, TrivialAddition) {
   EXPECT_EQ(1 + 1, 2);
 }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+TEST(StringTestSuite, HandlesStringView) {
+  constexpr string_view message = "Hello, Modern C++!";
+  
+  EXPECT_EQ(message.length(), 18);
+  EXPECT_TRUE(message.starts_with("Hello"));
+  EXPECT_FALSE(message.empty());
 }
+
+int main(int argc, char* argv[]) {
+  println("--- Running Google Test Suite ---");
+
+  ::testing::InitGoogleTest(&argc, argv);
+
+  const int result = RUN_ALL_TESTS();
+
+  println("--- Test execution finished. ---");
+
+  return result;
+}
+
